5e519d67d3bbeed4cc9c4e60844005a00d7ee4e6
Fahmi Ibrahim <129714988+fhmiibrhimdev@users.noreply.github.com>
2024-12-02 17:00:49 +0700

    Create latihan1-wag.c

---
+++ perulangan/latihan1-wag.c
@@ -0,0 +1,23 @@
+// program yang mencetak angka 1 sampai 10 menggunakan perulangan for.
+
+// sebelum perbaikan
+// #include <stdioh>
+
+// int main() {
+//     for (int i = 1; i <= 10; i++) {
+//         printf("%d\n", i)
+//     }
+//     return 0;
+// }
+
+// setelah perbaikan
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
